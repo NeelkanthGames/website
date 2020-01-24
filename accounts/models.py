be18a686485afffa97a565ef02d1a3f7b78684ab
moumita <mdas.160395@gmail.com>
2020-01-25 00:47:56 +0530

    changes to user

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 from django_countries.fields import CountryField
 
+user_type_category =(
+    ('user' , 'user'),
+    ('staff' , 'staff'),
+    ('admin' , 'admin'),
+)
 
 class UserProfileInfo(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
@@ -9,7 +14,7 @@ class UserProfileInfo(models.Model):
     country = CountryField(blank_label='Country')
     profile_pic = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
     is_subscribed = models.BooleanField(max_length=1,default=False,null=True)
-    is_staff = models.CharField(max_length=1,null=True)
+    user_type = models.CharField(max_length=20,choices=user_type_category,default='user')
     def __str__(self):
         return self.user.username
 
